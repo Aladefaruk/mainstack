9154718cc9f82d0931b9ee44bc5741cd19531f43
Faruk Alade <farukalade@Faruks-MBP.lan>
2024-04-21 04:57:12 -0400

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "./styles/styles.css";
+import "./styles/index.css";
+
+function App() {
+  return (
+    <div className=" bg-red-500">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
